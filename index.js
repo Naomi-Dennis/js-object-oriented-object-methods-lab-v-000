ece29ad93f7ce5e57c80978d0f288e61592cf3bb
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 15:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function BoardMember  (name, homeState, training){
     this.name = name
     this.homeState = homeState
     this.training = training
-  }
 
   function veto(){
     return "No, I must disagree"
