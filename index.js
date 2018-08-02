17d93b6c578e1d81a1ede6a7ad539eeddbbecbd6
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 15:56:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// 'use strict'
-// class BoardMember{
-//   constructor(name, homeState, training){
-//     this.name = name
-//     this.homeState = homeState
-//     this.training = training
-//   }
-//
-//   function veto(){
-//     return "No, I must disagree"
-//   }
-//
-//   function approve(){
-//     return "You can do that!"
-//   }
-//
-//   function doCharity(){
-//     return "I like to help people."
-//   }
-//
-//   function releasePressStatement(){
-//     return "You will see great things from Scuber."
-//   }
-//   function sayHi(){
-//     return `Hi, my name is ${this.name}. I am from <homestate>, and I was trained in ${this.training}.`
-//   }
-// }
+'use strict'
+class BoardMember{
+  constructor(name, homeState, training){
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
+
+  function veto(){
+    return "No, I must disagree"
+  }
+
+  function approve(){
+    return "You can do that!"
+  }
+
+  function doCharity(){
+    return "I like to help people."
+  }
+
+  function releasePressStatement(){
+    return "You will see great things from Scuber."
+  }
+  function sayHi(){
+    return `Hi, my name is ${this.name}. I am from <homestate>, and I was trained in ${this.training}.`
+  }
+}
