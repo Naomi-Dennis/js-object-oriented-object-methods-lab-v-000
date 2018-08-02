ca42d299a3b8b309d4af6f64bb86a729e5a74722
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 15:57:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 let boardMembers = []
-function BoardMember{
-  constructor(name, homeState, training){
+function BoardMember  (name, homeState, training){
     this.name = name
     this.homeState = homeState
     this.training = training
